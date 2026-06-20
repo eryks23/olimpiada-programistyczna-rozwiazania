4426d8d862d1383495394a1a700b875c75e26172
Eryk Mazurek <168019801+eryks23@users.noreply.github.com>
2026-06-20 19:53:42 +0200

    Refactor variable names for clarity in candidate power

--- wsp/10_great_ones/10_great_ones.py
+++ wsp/10_great_ones/10_great_ones.py
@@ -1,10 +1,10 @@
-liczba_kandydatów = int(input(""))
+candidate_count = int(input(""))
 
-if (1 <= liczba_kandydatów <= 1000000):
-    moce = list(map(int, input("").split()))
+if 1 <= candidate_count <= 1000000:
+    powers = list(map(int, input("").split()))
     
-    if not (-1000000 <= max(moce) <= 1000000):
-        print("Moc kandydata musi być z zakresu od -1000000 do 1000000")
+    if not (-1000000 <= max(powers) <= 1000000):
+        print("Candidate power must be within the range of -1000000 to 1000000")
     else:
-        sortowane = sorted(moce, reverse=True)[:10]
-        print(*sortowane)
+        sorted_powers = sorted(powers, reverse=True)[:10]
+        print(*sorted_powers)
